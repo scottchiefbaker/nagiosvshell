70d6c5655102023388df8d1c96e0f696db16a180
Scott Baker <scott.baker@directlink.coop>
2019-07-17 19:47:44 -0700

    HTML5 doctype

--- views/header.php
+++ views/header.php
@@ -61,21 +61,16 @@ function display_header($page_title='Nagios Visual Shell')
 	$coreurl        = COREURL;
 
 	$header = '
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<!DOCTYPE html>
+<html>
 <head profile="http://dublincore.org">
 <title>'.$page_title.'</title>
 
 <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
-<link rel="schema.DC" href="http://dublincore.org/2008/01/14/dcelements.rdf#" />
-<link rel="schema.DCTERMS" href="http://dublincore.org/2008/01/14/dcterms.rdf#" />
 <meta name="description" content="Nagios" />
-<meta name="keywords" content="Nagios" />
-<meta http-equiv="content-style-type" content="text/css" />
-<meta http-equiv="content-language" content="en" />
-<meta name="site" content="Nagios" />
+<meta name="keywords" content="Nagios VShell" />
 
-<link rel="stylesheet" href="'.$css_path.'" type="text/css" media="screen" />
+<link rel="stylesheet" href="' . $css_path . '" type="text/css" media="screen" />
 <style type="text/css">
 <!-- use external stylesheet to control page style -->
 </style>

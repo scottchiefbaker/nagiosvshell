1005cf987cbf0dee3c178da48e1d08b437db05a3
Scott Baker <scott.baker@directlink.coop>
2020-06-12 10:08:35 -0700

    Hostgroup cleanup
    View with -w because of whitespace fixes

--- views/hostgroups.php
+++ views/hostgroups.php
@@ -1,4 +1,4 @@
-<?php  //hostgroups.php    display page for hostgroup details 
+<?PHP  //hostgroups.php    display page for hostgroup details
 
 // Nagios V-Shell
 // Copyright (c) 2010 Nagios Enterprises, LLC.
@@ -10,12 +10,12 @@
 // the GNU General Public License. A copy of that license should have
 // been provided with this software, but in any event can be obtained
 // from http://www.fsf.org.
-// 
+//
 // This work is distributed in the hope that it will be useful, but
 // WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 // General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
@@ -30,7 +30,7 @@
 // licenses that may apply to the software.)
 //
 // Contributions to this software are subject to your understanding and acceptance of
-// the terms and conditions of the Nagios Contributor Agreement, which can be found 
+// the terms and conditions of the Nagios Contributor Agreement, which can be found
 // online at:
 //
 // http://www.nagios.com/legal/contributoragreement/
@@ -39,13 +39,13 @@
 // DISCLAIMER:
 //
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
-// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
-// PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+// PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
 // HOLDERS BE LIABLE FOR ANY CLAIM FOR DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
-// OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE 
+// OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
 // GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, STRICT LIABILITY, TORT (INCLUDING 
-// NEGLIGENCE OR OTHERWISE) OR OTHER ACTION, ARISING FROM, OUT OF OR IN CONNECTION 
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, STRICT LIABILITY, TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) OR OTHER ACTION, ARISING FROM, OUT OF OR IN CONNECTION
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 
@@ -74,19 +74,19 @@ function display_hostgroups($data)
 		</div>
 	</form>
 </div>";
-	
+
 	//$start_time = microtime(TRUE);
-	
-	//////////////////////////////////////table creation, displays both summary and grid view   
+
+	//////////////////////////////////////table creation, displays both summary and grid view
 	foreach($data as $group => $group_data)
 	{
-		$id = str_replace($bad_chars,'_',$group); 
-		//skip ahead if there are no authorized hosts			
-		if(array_sum($group_data['host_counts'])==0)  continue; //skip empty groups 
+		$id = str_replace($bad_chars,'_',$group);
+		//skip ahead if there are no authorized hosts
+		if(array_sum($group_data['host_counts'])==0)  continue; //skip empty groups
 
 		//group label
-		$page .= "<h5><a name=\"$group\">$group</a></h5>"; 
-		
+		$page .= "<div class=\"font-150\"><b><a name=\"$group\">$group</a></b></div>";
+
 		$page .= "<table class='statusable'><tr>
 				<th></th><th>Up</th><th>Down</th><th colspan='2'>Unreachable</th></tr>
 				<tr><td>Hosts</td>
@@ -94,43 +94,43 @@ function display_hostgroups($data)
 					<td class='down'> {$group_data['host_counts']['DOWN']} </td>
 					<td class='unreachable' colspan='2'> {$group_data['host_counts']['UNREACHABLE']} </td>
 				</tr>
-				
-				<tr><th></th><th>Ok</th><th>Warning</th><th>Critical</th><th>Unknown</th></tr>			
+
+				<tr><th></th><th>Ok</th><th>Warning</th><th>Critical</th><th>Unknown</th></tr>
 				<tr>
-					<td>Services</td>		 	
+					<td>Services</td>
 					<td class='ok'> {$group_data['service_counts']['OK']} </td>
 					<td class='warning'> {$group_data['service_counts']['WARNING']} </td>
 					<td class='critical'> {$group_data['service_counts']['CRITICAL']} </td>
 					<td class='unknown'> {$group_data['service_counts']['UNKNOWN']} </td>
 				</tr></table>";
-		
+
 		$page .= "<p class='label'><a onclick=\"showHide('$id')\" href='javascript:void(0)'>Toggle Grid</a></p>";
-		
-		//details table in GRID view 
+
+		//details table in GRID view
 		$page .= "<div class='hidden' id='$id'>";
 		$page .= "<table class='statustable'><tr><th>$group</th><th>Host Status</th><th>Services</th></tr>";
 
 		foreach($group_data['member_data'] as $member => $member_data)
 		{
 			$page .= "<tr>\n";
-	
+
 			//pull group member data from global $hosts array
 			$page .= "<td><a href='{$member_data['host_url']}'>".$member_data['host_name']."</a></td><td class='{$member_data['state_class']}'>".$member_data['host_state'].'</td>';
 			$page .= "<td>\n";
-	
-			//pull group member data from global $services array 
+
+			//pull group member data from global $services array
 			foreach($member_data['services'] as $service => $service_data)
 			{
-				$page .= " <span class='{$service_data['state_class']}'><a href='{$service_data['service_url']}'>".$service_data['description']."</a></span>&nbsp; ";				
-			}	
-			$page .= "</td></tr>";		
-		}	
+				$page .= " <span class='{$service_data['state_class']} service-status'><a href='{$service_data['service_url']}'>".$service_data['description']."</a></span>&nbsp; ";
+			}
+			$page .= "</td></tr>";
+		}
 		$page .= "</table></div><br />\n";
-		
+
 	}
-	
-	$page .= "</div> <!-- end contentWrapper -->"; 
-	
+
+	$page .= "</div> <!-- end contentWrapper -->";
+
 	//$end_time = microtime(TRUE);
 	//fb($end_time - $start_time, "Elapsed time in hostgroups");
 	return $page;

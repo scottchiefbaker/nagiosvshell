24fd0e961215c625bbfda819d0fcf1077440d985
Scott Baker <scott.baker@directlink.coop>
2023-07-12 16:10:54 -0700

    Allow filtering hosts by host_group

--- hosts.php
+++ hosts.php
@@ -7,13 +7,25 @@ $debug        = $_GET['debug']        ?? 0;
 $state_filter = $_GET['state_filter'] ?? "";
 $name_filter  = $_GET['name_filter']  ?? "";
 $host_filter  = $_GET['host_filter']  ?? "";
+$host_group   = $_GET['host_group']   ?? "";
 
-////////////////////////////////////////////////////////
+// Look for a specific hostgroup to show
+if ($host_group) {
+	$hosts = $v->get_hostgroup_members($host_group);
 
-$host_info = $v->get_all_hosts();
+	$svc_info = [];
+	foreach ($hosts as $x) {
+		$y                = $v->get_host_data($x);
+		$name             = $y['host_name'];;
+		$host_info[$name] = array_merge($svc_info, $y);
+	}
+// Get hosts after filtering
+} else {
+	$host_info = $v->get_all_hosts($state_filter, $name_filter, $host_filter);
+}
 
-print $v->sluz->assign("show_host_table", true);
-print $v->sluz->assign("host_info", $host_info);
+$v->sluz->assign("show_host_table", true);
+$v->sluz->assign("host_info", $host_info);
 
 print $v->fetch("hosts.stpl");
 

2c2f61faa193e92e190265761c76f8ce92aec81b
Scott Baker <scott.baker@directlink.coop>
2023-07-12 16:10:15 -0700

    Allow filtering services by host_group

--- services.php
+++ services.php
@@ -8,11 +8,23 @@ $debug = $_GET['debug'] ?? 0;
 $state_filter = $_GET['state_filter'] ?? "";
 $name_filter  = $_GET['name_filter']  ?? "";
 $host_filter  = $_GET['host_filter']  ?? "";
+$host_group   = $_GET['host_group']   ?? "";
 
 ////////////////////////////////////////////////////////
 
+// Look for a specific hostgroup to show
+if ($host_group) {
+	$host = $v->get_hostgroup_members($host_group);
+
+	$svc_info = [];
+	foreach ($host as $x) {
+		$y        = $v->get_all_services($state_filter, $name_filter, $x);
+		$svc_info = array_merge($svc_info, $y);
+	}
 // Get services after filtering
-$svc_info = $v->get_all_services($state_filter, $name_filter, $host_filter);
+} else {
+	$svc_info = $v->get_all_services($state_filter, $name_filter, $host_filter);
+}
 
 $v->sluz->assign("show_service_table", true);
 $v->sluz->assign("service_info", $svc_info);

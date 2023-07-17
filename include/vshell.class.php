61ae5d2b036849d0ed81dcb02f513cd899be3483
Scott Baker <scott.baker@directlink.coop>
2023-07-17 10:01:13 -0700

    Catch "PROBLEMS" better

--- include/vshell.class.php
+++ include/vshell.class.php
@@ -138,8 +138,12 @@ class vshell {
 				$host_name = $x['host_name']           ?? "";
 				$svc_name  = $x['service_description'] ?? "";
 
+				// If we're looking for a specific state we catch it here
+				// "PROBLEMS" is a specific case where anything that is not "OK" is flagged
+				$state_filter_match = ($state_filter === $state_str) || (($state_filter === "PROBLEMS") && ($state_str !== "OK"));
+
 				// Apply various filters
-				if ($state_filter && ($state_filter === $state_str)) {
+				if ($state_filter && $state_filter_match) {
 					$ret[$host_name][$svc_name] = $x;
 				} elseif ($host_filter && ($host_filter === $host_name)) {
 					$ret[$host_name][$svc_name] = $x;
